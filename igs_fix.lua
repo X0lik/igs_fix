265c76ba9a8f5d28f309ce99905d65f0688361f9
X0lik <80484098+X0lik@users.noreply.github.com>
2023-08-05 09:21:44 +0300

    Added DarkRP support

--- igs_fix.lua
+++ igs_fix.lua
@@ -1,6 +1,15 @@
+local gmTeams = {}
+hook.Add( "PostGamemodeLoaded", "IGS:JobFix", function()
+	if next( RPExtraTeams ) ~= nil then
+		gmTeams = RPExtraTeams
+	elseif rp and rp.teams and next( rp.teams ) ~= nil then
+		gmTeams = rp.teams
+	end
+end)
+
 hook.Remove("playerCanChangeTeam", "IGS" )
 hook.Add("playerCanChangeTeam", "IGS", function(pl, iTeam, bForce)
-	local TEAM = rp.teams[iTeam]
+	local TEAM = gmTeams[iTeam]
 	local ITEM = IGS.PlayerHasOneOf(pl, IGS.ITEMS.DRP.JOBS[TEAM.command])
 	if ITEM ~= nil then -- донатный итем
 		local allow, message = hook.Run("IGS.playerCanChangeTeam", pl, iTeam, bForce)
